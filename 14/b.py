ffeee77172cf96c96ab1b6f89e3536615ec662f9
Ridho Pratama <p.ridho@yahoo.co.id>
2018-12-15 00:56:00 +0700

    Fixed day14b

--- 14/b.py
+++ 14/b.py
@@ -1,5 +1,4 @@
 from blist import blist
-import tqdm
 
 problem = 9
 recipes = blist([3, 7])
@@ -14,13 +13,15 @@ while True:
     for x in res:
         recipes.append(x)
 
+        last = ''.join(f'{x}' for x in recipes[-len(check):])
+
+        if last == check:
+            print(len(recipes) - len(check))
+            exit()
+            break
+
     idx1 = (1 + recipes[idx1] + idx1) % len(recipes)
     idx2 = (1 + recipes[idx2] + idx2) % len(recipes)
 
-    last = ''.join(f'{x}' for x in recipes[-len(check):])
-
-    if last == check:
-        print(len(recipes) - len(check))
-        break
 
 print(''.join(f'{x}' for x in recipes[-10:]))
